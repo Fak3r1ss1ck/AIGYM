022bd0bc8b70f61212c995816d19656fd8f9480c
Fak3r1ss1ck <mail4projectsonly2me@gmail.com>
2024-11-04 14:59:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, redirect, url_for
 import subprocess
+import os  # Import os to access environment variables
 
 app = Flask(__name__)
-
 
 # Route for the main menu
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 # Route to handle the selected exercise
 @app.route('/exercise/<exercise_name>')
@@ -21,12 +20,12 @@ def exercise(exercise_name):
     else:
         return "Exercise not found!", 404
 
-
 # Back to menu when 'esc' is pressed
 @app.route('/back_to_menu')
 def back_to_menu():
     return redirect(url_for('index'))
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable provided by Render, default to 5000 if not set
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)  # Bind to 0.0.0.0 and use the port variable
